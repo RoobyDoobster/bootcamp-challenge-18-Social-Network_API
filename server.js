3350eb943c99d99029c7d72d734b79bdf6804b0d
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-03-23 20:11:32 -0700

    debugging

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://loclahost/social-network-API', {
-    userFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:Ruby10879@cluster0.qlo0aex.mongodb.net/social-network-API', {
+    // userFindAndModify: false,
     useNewUrlParser: true,
-    UseUnifiedTopology: true
+    // UseUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log('Connected on localhost:${PORT}'));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
